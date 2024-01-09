8af2109456b5b2b0056b68d0294157a7faa3fba0
Guillaume <guillaume.dupuy@ynov.com>
2024-01-10 00:58:42 +0100

    add new functions

--- pbm.go
+++ pbm.go
@@ -50,11 +50,13 @@ func ReadPBM(filename string) (*PBM, error) {
 		data[i] = make([]bool, pbm.Width)
 	}
 
+	pbm.Data = data
+
 	switch pbm.MagicNumber {
 	case "P1":
 		for i := 0; i < pbm.Height; i++ {
 			scanner.Scan()
-			line := strings.Fields(scanner.Text()) // Split the line into fields
+			line := strings.Fields(scanner.Text())
 			for j, val := range line {
 				if val == "1" {
 					data[i][j] = true
@@ -78,4 +80,111 @@ func ReadPBM(filename string) (*PBM, error) {
 	}
 
 	return &pbm, nil
+}
+
+// Size returns the width and height of the image.
+func (pbm *PBM) Size() (int,int){
+	return pbm.Width, pbm.Height
+}
+
+// At returns the value of the pixel at (x, y).
+func (pbm *PBM) At(x, y int) bool{
+	return pbm.Data[y][x]
+}
+
+// Set sets the value of the pixel at (x, y).
+func (pbm *PBM) Set(x, y int, value bool){
+	pbm.Data[y][x] = value
+}
+
+// Save saves the PBM image to a file and returns an error if there was a problem.
+func (pbm *PBM) Save(filename string) error{
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, "%s\n", pbm.MagicNumber)
+	fmt.Fprintf(file, "%d %d\n", pbm.Width, pbm.Height)
+
+	switch pbm.MagicNumber {
+	case "P1":
+		for i := 0; i < pbm.Height; i++ {
+			for j := 0; j < pbm.Width; j++ {
+				if pbm.Data[i][j] {
+					fmt.Fprintf(file, "1 ")
+				} else {
+					fmt.Fprintf(file, "0 ")
+				}
+			}
+			fmt.Fprintln(file)
+		}
+	case "P4":
+		for i := 0; i < pbm.Height; i++ {
+			for j := 0; j < pbm.Width; j++ {
+				if pbm.Data[i][j] {
+					fmt.Fprintf(file, "1")
+				} else {
+					fmt.Fprintf(file, "0")
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Invert inverts the colors of the PBM image.
+func (pbm *PBM) Invert(){
+	for i := 0; i < pbm.Height; i++ {
+		for j := 0; j < pbm.Width; j++ {
+			pbm.Data[i][j] = !pbm.Data[i][j]
+		}
+	}
+}
+
+// Flip flips the PBM image horizontally.
+func (pbm *PBM) Flip(){
+	for i := 0; i < pbm.Height; i++ {
+		for j := 0; j < pbm.Width/2; j++ {
+			pbm.Data[i][j], pbm.Data[i][pbm.Width-1-j] = pbm.Data[i][pbm.Width-1-j], pbm.Data[i][j]
+		}
+	}
+}
+
+// Flop flops the PBM image vertically.
+func (pbm *PBM) Flop(){
+	for i := 0; i < pbm.Height/2; i++ {
+		for j := 0; j < pbm.Width; j++ {
+			pbm.Data[i][j], pbm.Data[pbm.Height-1-i][j] = pbm.Data[pbm.Height-1-i][j], pbm.Data[i][j]
+		}
+	}
+}
+
+// SetMagicNumber sets the magic number of the PBM image and change the data format.
+func (pbm *PBM) SetMagicNumber(magicNumber string){
+	pbm.MagicNumber = magicNumber
+	switch magicNumber {
+	case "P1":
+		for i := 0; i < pbm.Height; i++ {
+			for j := 0; j < pbm.Width; j++ {
+				if pbm.Data[i][j] {
+					pbm.Data[i][j] = true
+				} else {
+					pbm.Data[i][j] = false
+				}
+			}
+		}
+	case "P4":
+		for i := 0; i < pbm.Height; i++ {
+			for j := 0; j < pbm.Width; j++ {
+				if pbm.Data[i][j] {
+					pbm.Data[i][j] = true
+				} else {
+					pbm.Data[i][j] = false
+				}
+			}
+		}
+	}
 }
